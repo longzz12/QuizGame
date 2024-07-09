29155c62c9ba1acb9f993f009068daf3771b59c6
longzz12 <kimlong26012004@gmail.com>
2024-07-09 20:05:33 +0700

    Project Rupp

---
+++ src/QuizGame/Main.java
@@ -0,0 +1,15 @@
+package QuizGame;
+
+import javax.swing.SwingUtilities;
+
+public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                Quiz quiz = new Quiz();
+            }
+        });
+    }
+}
+
